2483eb6787a52380966adabb8fea743146041439
舒辞落樱 <ylx963304364@163.com>
2023-10-07 17:49:38 +0800

    编辑员工

--- src/com/yang/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ src/com/yang/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -21,6 +21,7 @@ public interface EmployeeMapper {
 
     /**
      * 新增员工
+     *
      * @param employee
      */
     @Insert("insert into employee (name,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status)" +
@@ -29,6 +30,7 @@ public interface EmployeeMapper {
 
     /**
      * 分页查询
+     *
      * @param employeePageQueryDTO
      * @return
      */
@@ -36,7 +38,17 @@ public interface EmployeeMapper {
 
     /**
      * 根据主键动态修改属性
+     *
      * @param employee
      */
     void update(Employee employee);
+
+    /**
+     * 根据Id查询员工信息
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(long id);
 }
